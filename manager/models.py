3380be6944724587d7b2390954645d963930de4a
colajam93 <fs.fake.g1@gmail.com>
2016-04-25 03:04:51 +0900

    add models

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+FIELD_LENGTH = 100
+
+
+class Package(models.Model):
+    name = models.CharField(max_length=FIELD_LENGTH)
+    source = models.CharField(max_length=FIELD_LENGTH)
+
+
+class Build(models.Model):
+    version = models.CharField(max_length=FIELD_LENGTH)
+    date = models.DateTimeField()
+    package = models.ForeignKey(Package, default='')
