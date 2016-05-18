50b8fb2d0d359a1d31997fe214edeac2cbcc63e1
colajam93 <fs.fake.g1@gmail.com>
2016-05-18 15:15:39 +0900

    Add package list

--- manager/views.py
+++ manager/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from manager.models import Package
+
 
 def package_list(request):
+    packages = Package.objects.all().order_by('id')
+    return render(request, 'package_list.html', {'packages': packages})
     return HttpResponse('Package list')
 
 
