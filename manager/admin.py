b7eb633870b979a386b59d338bf3fb14b4c95620
colajam93 <fs.fake.g1@gmail.com>
2016-05-18 16:20:48 +0900

    Update admin page

--- manager/admin.py
+++ manager/admin.py
@@ -3,14 +3,14 @@ from manager.models import Package, Build
 
 
 class PackageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'source')
+    list_display = ('id', 'name', 'source')
 
 
 admin.site.register(Package, PackageAdmin)
 
 
 class BuildAdmin(admin.ModelAdmin):
-    list_display = ('package', 'version', 'date')
+    list_display = ('id', 'package', 'version', 'date')
 
 
 admin.site.register(Build, BuildAdmin)
