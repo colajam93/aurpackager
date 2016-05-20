d7ac397b74f8fd77d5da60d23bce7cefc75c4d6a
colajam93 <fs.fake.g1@gmail.com>
2016-05-20 14:20:24 +0900

    Update Build model

--- manager/models.py
+++ manager/models.py
@@ -15,6 +15,14 @@ class Build(models.Model):
     package = models.ForeignKey(Package, default='')
     version = models.CharField(max_length=FIELD_LENGTH)
     date = models.DateTimeField()
+    result_path = models.CharField(max_length=FIELD_LENGTH, default='')
+    log_path = models.CharField(max_length=FIELD_LENGTH, default='')
+    STATUS_CHOICES = (
+        ('BUILDING', 'Building'),
+        ('SUCCESS', 'Success'),
+        ('FAILURE', 'Failure')
+    )
+    status = models.CharField(max_length=FIELD_LENGTH, choices=STATUS_CHOICES, default=STATUS_CHOICES[0])
 
     def __str__(self):
         return '{} {} {}'.format(self.package, self.version, self.date)
