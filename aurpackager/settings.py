fa828bdcf2d7a29172b7ffcac3572f05ea929abd
colajam93 <fs.fake.g1@gmail.com>
2016-05-21 01:05:05 +0900

    Fix time zone

--- aurpackager/settings.py
+++ aurpackager/settings.py
@@ -101,9 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
