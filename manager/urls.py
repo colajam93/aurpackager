4087333fde8e1158dbd2d197bfbc0f67d01f0a6d
colajam93 <fs.fake.g1@gmail.com>
2016-05-21 20:40:50 +0900

    Replace package id by package name

--- manager/urls.py
+++ manager/urls.py
@@ -3,7 +3,7 @@ from manager import views
 
 urlpatterns = [
     url(r'^$', views.package_list, name='package_list'),
-    url(r'^(?P<package_id>\d+)/$', views.package_detail, name='package_detail'),
-    url(r'^(?P<package_id>\d+)/build/$', views.package_build, name='package_build'),
-    url(r'^(?P<package_id>\d+)/(?P<build_id>\d+)/$', views.build_detail, name='build_detail')
+    url(r'^packages/(?P<package_name>[a-zA-Z0-9_+-]+)/$', views.package_detail, name='package_detail'),
+    url(r'^packages/(?P<package_name>[a-zA-Z0-9_+-]+)/build/$', views.package_build, name='package_build'),
+    url(r'^packages/(?P<package_name>[a-zA-Z0-9_+-]+)/(?P<build_number>\d+)/$', views.build_detail, name='build_detail')
 ]
--- manager/views.py
+++ manager/views.py
@@ -8,24 +8,29 @@ def package_list(request):
     return render(request, 'package_list.html', {'packages': packages})
 
 
-def package_detail(request, package_id):
-    builds = Build.objects.filter(package_id=package_id).order_by('-date')
+def package_detail(request, package_name):
+    package = Package.objects.get(name=package_name)
+    builds = Build.objects.filter(package_id=package.id).order_by('-date')
     for build, number in zip(builds, range(1, len(builds) + 1)):
         build.number = number
-    return render(request, 'package_detail.html', {'package_id': package_id, 'builds': builds})
+    return render(request, 'package_detail.html', {'package_name': package_name, 'builds': builds})
 
 
-def package_build(request, package_id):
-    package = Package.objects.get(id=package_id)
+def package_build(request, package_name):
+    package = Package.objects.get(name=package_name)
     if package:
-        BuilderManager().register(package_id)
+        BuilderManager().register(package.id)
         return render(request, 'package_build.html', {'package': package})
     else:
         return redirect('manager:package_list')
 
 
-def build_detail(request, package_id, build_id):
-    build = Build.objects.get(id=build_id)
+def build_detail(request, package_name, build_number):
+    package = Package.objects.get(name=package_name)
+    try:
+        build = Build.objects.filter(package_id=package.id).order_by('-date')[int(build_number) - 1]
+    except IndexError:
+        build = None
     if build:
         return render(request, 'build_detail.html', {'build': build, 'package': build.package})
     else:
