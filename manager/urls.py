18bd0bcc0d892aef4ea9babfc6ec2af6e40cea62
colajam93 <fs.fake.g1@gmail.com>
2016-05-21 20:43:48 +0900

    Add alternative package list url

--- manager/urls.py
+++ manager/urls.py
@@ -3,6 +3,7 @@ from manager import views
 
 urlpatterns = [
     url(r'^$', views.package_list, name='package_list'),
+    url(r'^packages/$', views.package_list, name='package_list'),
     url(r'^packages/(?P<package_name>[a-zA-Z0-9_+-]+)/$', views.package_detail, name='package_detail'),
     url(r'^packages/(?P<package_name>[a-zA-Z0-9_+-]+)/build/$', views.package_build, name='package_build'),
     url(r'^packages/(?P<package_name>[a-zA-Z0-9_+-]+)/(?P<build_number>\d+)/$', views.build_detail, name='build_detail')
