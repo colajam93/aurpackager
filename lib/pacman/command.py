c8a96c2e1fc193bdde8cf307d7e60229d7a1cc0d
colajam93 <fs.fake.g1@gmail.com>
2016-05-31 06:22:44 +0900

    Fix zombie process

--- lib/pacman/command.py
+++ lib/pacman/command.py
@@ -3,7 +3,8 @@ import subprocess
 
 def execute(command, discard_output=False):
     if discard_output:
-        return subprocess.Popen(command, shell=True, close_fds=True, stdin=None, stdout=None, stderr=None)
+        p = subprocess.run(command, shell=True, stdin=None, stdout=None, stderr=None)
+        p.wait()
     else:
         return subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                               universal_newlines=True)
