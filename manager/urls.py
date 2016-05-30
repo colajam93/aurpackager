93980bd2b0e124b0898a9f90b3766d0fb4718557
colajam93 <fs.fake.g1@gmail.com>
2016-05-30 16:32:45 +0900

    Refactor

--- manager/urls.py
+++ manager/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import url
 from manager import views
 
-__PACKAGE_NAME_REGEX = r'(?P<package_name>[a-zA-Z0-9_+-]+)'
+_PACKAGE_NAME_REGEX = r'(?P<package_name>[a-zA-Z0-9_+-]+)'
 
 urlpatterns = [
     url(r'^$', views.package_list, name='package_list'),
     url(r'^packages/$', views.package_list, name='package_list'),
     url(r'^packages/register/$', views.package_register, name='package_register'),
-    url(r'^packages/register/{}/$'.format(__PACKAGE_NAME_REGEX), views.package_register_detail,
+    url(r'^packages/register/{}/$'.format(_PACKAGE_NAME_REGEX), views.package_register_detail,
         name='package_register_detail'),
-    url(r'^packages/(?P<package_name>[a-zA-Z0-9_+-]+)/$', views.package_detail, name='package_detail'),
-    url(r'^packages/(?P<package_name>[a-zA-Z0-9_+-]+)/(?P<build_number>\d+)/$', views.build_detail,
+    url(r'^packages/{}/$'.format(_PACKAGE_NAME_REGEX), views.package_detail, name='package_detail'),
+    url(r'^packages/{}/(?P<build_number>\d+)/$'.format(_PACKAGE_NAME_REGEX), views.build_detail,
         name='build_detail'),
-    url(r'^packages/(?P<package_name>[a-zA-Z0-9_+-]+)/(?P<build_number>\d+)/download/$', views.build_download,
+    url(r'^packages/{}/(?P<build_number>\d+)/download/$'.format(_PACKAGE_NAME_REGEX), views.build_download,
         name='build_download'),
 ]
