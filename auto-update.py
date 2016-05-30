b971ae52f7de34aaf09816a4079c324e8045fcde
colajam93 <fs.fake.g1@gmail.com>
2016-05-31 03:37:50 +0900

    Add auto update script

---
+++ auto-update.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import time
+import urllib.request
+
+
+def main():
+    url = 'http://localhost:8000/api/build_all/?only_update=true'
+    interval = 1800  # 30 minutes
+
+    while True:
+        urllib.request.urlopen(url)
+        time.sleep(interval)
+
+
+if __name__ == '__main__':
+    main()
