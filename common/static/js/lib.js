eb71955693f5397de9a3b30d2b5c2f1bb31259ad
colajam93 <fs.fake.g1@gmail.com>
2016-06-26 05:58:26 +0900

    Revert "Separate js"
    This reverts commit dc05470c74c533538f0a33d581d3d277eda95240.

--- common/static/js/lib.js
+++ common/static/js/lib.js
@@ -29,15 +29,3 @@ function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
-
-$.ajaxSetup({
-    beforeSend: function (xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
-    }
-});
-
-$(function () {
-    enableTableRowClick();
-});
