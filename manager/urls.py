66bac226b234c5620bec8b6d4d39ee7607afcfd1
colajam93 <fs.fake.g1@gmail.com>
2016-07-24 00:43:20 +0900

    Always use url temlpate tag

--- manager/urls.py
+++ manager/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from manager import views
 
 _PACKAGE_NAME_REGEX = r'(?P<package_name>[a-zA-Z0-9_+-]+)'
+_PACKAGE_NAME_REGEX_WITH_EMPTY = r'(?P<package_name>[a-zA-Z0-9_+-]*)'
 _BUILD_NUMBER_REGEX = r'(?P<build_number>\d+)'
 _REGEX = {'name': _PACKAGE_NAME_REGEX, 'number': _BUILD_NUMBER_REGEX}
 
@@ -9,7 +10,7 @@ urlpatterns = [
     url(r'^$', views.package_list, name='package_list'),
     url(r'^packages/$', views.package_list, name='package_list'),
     url(r'^packages/register/$', views.package_register, name='package_register'),
-    url(r'^packages/register/{name}/$'.format(**_REGEX), views.package_register_detail,
+    url(r'^packages/register/{name}$'.format(name=_PACKAGE_NAME_REGEX_WITH_EMPTY), views.package_register_detail,
         name='package_register_detail'),
     url(r'^packages/{name}/$'.format(**_REGEX), views.package_detail, name='package_detail'),
     url(r'^packages/{name}/{number}/$'.format(**_REGEX), views.build_detail,
