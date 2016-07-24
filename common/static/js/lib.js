83da7b958f7ce120afcf7489e674d45d499197c1
colajam93 <fs.fake.g1@gmail.com>
2016-07-24 13:14:00 +0900

    Convert es6 code with babel

--- common/static/js/lib.js
+++ common/static/js/lib.js
@@ -1,22 +1,23 @@
+'use strict';
+
 // enable row click
 function enableTableRowClick() {
-    $('tr[data-href]').addClass('clickable')
-        .click(function (e) {
-            if (!$(e.target).is('a')) {
-                window.location = $(e.target).closest('tr').data('href');
-            }
-        });
+    $('tr[data-href]').addClass('clickable').click(function (e) {
+        if (!$(e.target).is('a')) {
+            window.location = $(e.target).closest('tr').data('href');
+        }
+    });
 }
 
 // https://docs.djangoproject.com/en/1.9/ref/csrf/#ajax
 function getCookie(name) {
-    let cookieValue = null;
+    var cookieValue = null;
     if (document.cookie && document.cookie != '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
             // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+            if (cookie.substring(0, name.length + 1) == name + '=') {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
             }
@@ -27,5 +28,7 @@ function getCookie(name) {
 
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method)
+    );
 }
+
