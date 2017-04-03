9b18296e48cc31159a7ef145b9c1dc3c8c8a6310
colajam93 <fs.fake.g1@gmail.com>
2017-04-04 00:17:50 +0900

    Add unofficial AUR server setting

---
+++ packager/settings_util.py
@@ -0,0 +1,5 @@
+class AURServer:
+    def __init__(self, address: str, user: str = '', password: str = ''):
+        self.address = address
+        self.user = user
+        self.password = password
