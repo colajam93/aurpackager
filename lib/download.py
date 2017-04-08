935fb43320f823f04a510d83e76df2126ed64609
colajam93 <fs.fake.g1@gmail.com>
2017-04-09 05:55:30 +0900

    Add opener support to lib.download.save_to_file

--- lib/download.py
+++ lib/download.py
@@ -1,8 +1,15 @@
-from urllib.request import urlopen
 from contextlib import closing
+from urllib.request import urlopen, OpenerDirector
 
 
-def save_to_file(url, file_path):
-    with closing(urlopen(url)) as request:
+def save_to_file(url: str, file_path: str, opener: OpenerDirector = None):
+    def process(request_):
         with open(file_path, 'wb') as f:
-            f.write(request.read())
+            f.write(request_.read())
+
+    if opener:
+        with closing(opener.open(url)) as request:
+            process(request)
+    else:
+        with closing(urlopen(url)) as request:
+            process(request)
