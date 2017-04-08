80a2628dafb41ba2a288e04eab3d15bcbae00365
colajam93 <fs.fake.g1@gmail.com>
2017-04-09 03:24:51 +0900

    Add AUR server argument to all aur query method

--- lib/aur.py
+++ lib/aur.py
@@ -6,20 +6,37 @@ from contextlib import closing
 from urllib.parse import quote
 from urllib.request import urlopen
 
-from typing import List
+from typing import List, Dict
 
 from lib.download import save_to_file
 from manager.models import Package
 from packager.settings_local import UNOFFICIAL_AUR_SERVERS
+from packager.settings_util import AURServer
 
-AUR_URL = 'https://aur.archlinux.org'
-BASE_URL = AUR_URL + '/rpc/?v=5&'
-INFO_URL = BASE_URL + 'type=info&'
-SEARCH_URL = BASE_URL + 'type=search&'
+_AUR_SERVERS: Dict[str, AURServer] = {Package.OFFICIAL: AURServer('https://aur.archlinux.org')}
+_AUR_SERVERS.update(UNOFFICIAL_AUR_SERVERS)
 
 
-def aur_package_url(name):
-    return AUR_URL + '/packages/{}'.format(name)
+def _aur_url(aur_server_tag: str) -> str:
+    # TODO: error report for unrecognized AUR server tag
+    server = _AUR_SERVERS.get(aur_server_tag, _AUR_SERVERS[Package.OFFICIAL])
+    return server.address
+
+
+def _base_url(aur_server_tag: str) -> str:
+    return _aur_url(aur_server_tag) + '/rpc/?v=5&'
+
+
+def _info_url(aur_server_tag: str) -> str:
+    return _base_url(aur_server_tag) + 'type=info&'
+
+
+def _search_url(aur_server_tag: str) -> str:
+    return _base_url(aur_server_tag) + 'type=search&'
+
+
+def package_url(aur_server_tag: str, package_name: str) -> str:
+    return _aur_url(aur_server_tag) + '/packages/{}'.format(package_name)
 
 
 class PackageNotFoundError(Exception):
@@ -37,34 +54,34 @@ class AttrDict(dict):
 
 
 class AURInfo(AttrDict):
-    def __init__(self, package_dict):
+    def __init__(self, package_dict: Dict[str, str], aur_server_tag: str) -> None:
         super().__init__(package_dict)
-        self.tar_url = AUR_URL + self.URLPath
-        self.aur_url = aur_package_url(self.Name)
+        self.tar_url = _aur_url(aur_server_tag) + self.URLPath
+        self.aur_url = package_url(aur_server_tag, self.Name)
 
 
 class DetailAURInfo(AURInfo):
-    def __init__(self, package_dict, pkgnames):
-        super().__init__(package_dict)
+    def __init__(self, package_dict: Dict[str, str], pkgnames: List[str], aur_server_tag: str) -> None:
+        super().__init__(package_dict, aur_server_tag)
         self.pkgnames = pkgnames
 
 
-def _aur_query(url):
+# TODO: add type hint(maybe complex)
+def _aur_query(url: str):
     with closing(urlopen(quote(url, safe='/?:&=[]'))) as request:
-        result = json.loads(request.read().decode())
-    return result
+        return json.loads(request.read().decode())
 
 
-def info(package: str) -> AURInfo:
-    url = INFO_URL + 'arg[]={}'.format(package)
+def info(package: str, aur_server_tag: str) -> AURInfo:
+    url = _info_url(aur_server_tag) + 'arg[]={}'.format(package)
     result = _aur_query(url)
     if result['resultcount'] == 0:
         raise PackageNotFoundError
-    return AURInfo(result['results'][0])
+    return AURInfo(result['results'][0], aur_server_tag)
 
 
-def detail_info(package: str) -> DetailAURInfo:
-    info_ = info(package)
+def detail_info(package: str, aur_server_tag: str) -> DetailAURInfo:
+    info_ = info(package, aur_server_tag)
     with tempfile.TemporaryDirectory() as temp_dir:
         tar_path = os.path.join(temp_dir, 'tarball')
         save_to_file(info_.tar_url, tar_path)
@@ -89,16 +106,16 @@ def detail_info(package: str) -> DetailAURInfo:
             temp_file.flush()
             completed = subprocess.run(['bash', temp_file.name], universal_newlines=True, stdout=subprocess.PIPE)
             pkgnames = completed.stdout.strip().split('\n')
-    return DetailAURInfo(info_, pkgnames)
+    return DetailAURInfo(info_, pkgnames, aur_server_tag)
 
 
-def multiple_info(packages):
-    url = INFO_URL + '&'.join(map(lambda x: 'arg[]={}'.format(x), packages))
+def multiple_info(packages: List[str], aur_server_tag: str) -> Dict[str, AURInfo]:
+    url = _info_url(aur_server_tag) + '&'.join(map(lambda x: 'arg[]={}'.format(x), packages))
     result = _aur_query(url)
 
     # dict which key is the package name
     ret = dict()
-    for package in (AURInfo(x) for x in result['results']):
+    for package in (AURInfo(x, aur_server_tag) for x in result['results']):
         ret[package.Name] = package
         packages.remove(package.Name)
     # if package is not found, insert None instead
@@ -108,15 +125,15 @@ def multiple_info(packages):
     return ret
 
 
-def search(package):
-    url = SEARCH_URL + 'arg={}'.format(package)
+def search(package: str, aur_server_tag: str) -> List[AURInfo]:
+    url = _search_url(aur_server_tag) + 'arg={}'.format(package)
     result = _aur_query(url)
-    return [AURInfo(x) for x in result['results']]
+    return [AURInfo(x, aur_server_tag) for x in result['results']]
 
 
-def exist(package):
+def exist(package: str, aur_server_tag: str) -> bool:
     try:
-        info(package)
+        info(package, aur_server_tag)
     except PackageNotFoundError:
         return False
     else:
@@ -124,6 +141,4 @@ def exist(package):
 
 
 def servers() -> List[str]:
-    r = list(UNOFFICIAL_AUR_SERVERS.keys())
-    r.append(Package.OFFICIAL)
-    return r
+    return list(_AUR_SERVERS.keys())
